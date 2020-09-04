ec8463642efda9395bffce9b0edb14779273a788
Artem Eroshenko <eroshenkoam@me.com>
2020-09-04 15:17:14 +0300

    add suite

---
+++ src/test/java/io/qameta/allure/examples/junit4/DefaultSuite.java
@@ -0,0 +1,8 @@
+package io.qameta.allure.examples.junit4;
+
+import io.qameta.allure.junit4.AllureSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(AllureSuite.class)
+public class DefaultSuite {
+}
